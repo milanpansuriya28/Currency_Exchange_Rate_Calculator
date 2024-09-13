0d9892e1c22cdbef61804902ff1a133fe4315a3d
Milan H. Pansuriya <milanpansuriya2828@gmail.com>
2024-09-13 04:26:17 -0400

    All Done
    HTML, CSS and Jay Swaminarayan code writen for project

---
+++ currency/main.js
@@ -0,0 +1,35 @@
+const currencyEl_one = document.getElementById('currency-one')
+const currencyEl_two = document.getElementById('currency-two')
+const amountEl_one = document.getElementById('amount-one')
+const amountEl_two = document.getElementById('amount-two')
+
+const rateEl = document.getElementById('rate')
+const swap = document.getElementById('swap')
+
+function calculate(){
+    const currency_one = currencyEl_one.value;
+    const currency_two = currencyEl_two.value;
+
+    fetch(`https://v6.exchangerate-api.com/v6/eae5ddd256c4d6e94e12b4c3/latest/${currency_one}`)
+    .then((res) => res.json())
+    .then((data)=>{
+        const rate = data.conversion_rates[currency_two]
+        rate.innerText = `1 ${currency_one}= ${rate} ${currency_two}`
+
+        amountEl_two.value =(amountEl_one.value * rate).toFixed(2);
+
+        swap.addEventListener('click', () =>{
+            const temp = currencyEl_one.value;
+            currencyEl_one.value = currencyEl_two.value;
+            currencyEl_two.value =temp;
+        });
+    })
+}
+
+
+currencyEl_one.addEventListener('change', calculate);
+currencyEl_two.addEventListener('change', calculate);
+amountEl_one.addEventListener('input', calculate);
+amountEl_two.addEventListener('input', calculate);
+
+/*https://app.exchangerate-api.com/dashboard*/
